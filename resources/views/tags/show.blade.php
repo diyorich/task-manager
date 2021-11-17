0f6901729f3ffc4c89c98de15ab80b5136fc2ca7
Diyorich <netherdraker2001@gmail.com>
2021-11-18 02:35:49 +0500

    Finished first task with showing all task related to some tag

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -1,9 +1,18 @@
-<table border="1px" style="width: 50%">
+<button><a href="{{route('tasks.index')}}">Список тасков</a></button>
+
+
+<table border="2px" style="width: 80%">
+<tr><th>ID</th>
+<th>Title</th>
+<th>Date created</h>
+
 @foreach($tag->tasks as $related_tasks)
 <tr>
     <td>{{$related_tasks->id}}</td>
     <td>{{$related_tasks->title}}</td>
+    <td>{{$related_tasks->created_at}}</td>
     <br>
 </tr>
 @endforeach
 </table>
+
